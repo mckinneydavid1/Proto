d7e40d43d8442318ea5789289cc2d062d42612f3
dmmckinn <40120430+mckinneydavid1@users.noreply.github.com>
2019-07-16 15:26:21 -0400

    updated main

--- main.cpp
+++ main.cpp
@@ -4,3 +4,9 @@
 //Purpose: Allows a user to create either a business or consumer profile. If business, a
 //user can offer a deal. If consumer, user can utilize a deal made available by a business
 
+int main() {
+    
+    
+    
+    return 0;
+}
