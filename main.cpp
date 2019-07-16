f3962013140e753c39a61f2aac8ac010d8b88b18
dmmckinn <dmmckinn@umich.edu>
2019-07-16 15:11:38 -0400

    Initial commit. Starter files. Have to add function stubs.

---
+++ main.cpp
@@ -0,0 +1,6 @@
+//Author: David McKinney
+//Created: July 16, 2019
+//Modified: July 16, 2019
+//Purpose: Allows a user to create either a business or consumer profile. If business, a
+//user can offer a deal. If consumer, user can utilize a deal made available by a business
+
